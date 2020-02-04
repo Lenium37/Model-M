b15b5b721f11806adb08d188a9adbf1b207b29e7
Johannes Hirth <hirth.johannes@gmx.de>
2020-02-04 20:20:50 +0100

    working with own line detection

--- rgb_test.ino/rgb_test.ino.ino
+++ rgb_test.ino/rgb_test.ino.ino
@@ -1,7 +1,7 @@
 #include <Pixy2.h>
 #include <Wire.h> // I2Cs
 
-#define THRESHOLD_GRAY 45
+#define THRESHOLD_GRAY 90
 #define MIN_TRACK_WIDTH 35
 #define I2C_ADDRESS_OF_SLAVE 8
 #define I2C_READY_PIN 3
@@ -29,8 +29,12 @@ int battery_low_counter = 0;
 int index_of_left_line = 0;
 int index_of_right_line = 62;
 
+bool currently_seeing_both_lines = false;
+bool currently_seeing_only_left_line = false;
+bool currently_seeing_only_right_line = false;
+
 void debug(String s) {
-  Serial.print(s);
+  Serial1.print(s);
   // if changing Serial number don't forget to change it in setup() as well!
 }
 
@@ -90,7 +94,9 @@ int return_map(int value_to_map, int in_min, int in_max, int out_min, int out_ma
 }
 
 int calculate_pull_towards_ideallinie_in_degrees(int distance_from_ideallinie) {
-  int angle = return_map(distance_from_ideallinie, 0, 20, 0, 450);
+  //int angle = return_map(distance_from_ideallinie, 0, 20, 0, 450);
+  debug("distance_from_ideallinie: " + String(distance_from_ideallinie) + "\n");
+  int angle = return_map(distance_from_ideallinie, 0, 70, 0, 1350);
   if(angle > 450)
     angle = 450;
 
@@ -214,8 +220,57 @@ uint8_t center_of_drivable_space(uint8_t camera_line[63], int &track_till_border
   return center;
 }
 
-void update_indexes_of_lines() {
-  for(int i = 0; i rgb_at_y_75
+
+uint8_t count_lines(uint8_t array[63]) {
+  uint8_t number_of_lines = 0;
+  bool currently_in_line = false;
+
+  for(int i = 0; i < 63; i++) {
+    if(!currently_in_line && array[i] == 0) {
+      number_of_lines++;
+      currently_in_line = true;
+    }
+    if(array[i] == 1)
+      currently_in_line = false;
+
+  }
+  return number_of_lines;
+}
+
+void update_indexes_of_lines(uint8_t array[63]) {
+  for(int i = 32; i >= 0; i--) {
+    if(array[i] == 0)
+      index_of_left_line = i;
+  }
+  for(int i = 32; i < 63; i++) {
+    if(array[i] == 0)
+      index_of_right_line = i;
+  }
+}
+
+void detect_if_left_or_right_line(uint8_t array[63]) {
+  int index_of_line = 0;
+  for(int i = 0; i < 63; i++) {
+    if(array[i] == 0) {
+      index_of_line = i;
+      if(i > 32)
+        break;
+    }
+  }
+  int distance_from_left_index = abs(index_of_left_line - index_of_line);
+  int distance_from_right_index = abs(index_of_right_line - index_of_line);
+  if(distance_from_right_index <= distance_from_left_index) {
+    index_of_right_line = index_of_line;
+    currently_seeing_both_lines = false;
+    currently_seeing_only_left_line = false;
+    currently_seeing_only_right_line = true;
+  } else {
+    index_of_left_line = index_of_line;
+    currently_seeing_both_lines = false;
+    currently_seeing_only_left_line = true;
+    currently_seeing_only_right_line = false;
+  }
+
 }
 
 void loop() {
@@ -259,7 +314,7 @@ void loop() {
     else
       rgb_at_y_25[j] = 0;*/
 
-    //debug(String(gray) + "\n");
+    
 
 
     pixy.video.getRGB(i, 75, &r, &g, &b, false);
@@ -269,6 +324,7 @@ void loop() {
     else
       rgb_at_y_75[j] = 0;
 
+debug(String(gray) + "\n");
 
     /*pixy.video.getRGB(i, 125, &r, &g, &b, false);
     gray = 0.299 * r + 0.587 * g + 0.114 * b;
@@ -286,170 +342,210 @@ void loop() {
       rgb_at_y_175[j] = 0;*/
   }
 
-  update_indexes_of_lines();
-
-
-  /*c175 = center_of_drivable_space(rgb_at_y_175, line_175_until_border, look_towards_right_or_left);
-  if(c175 < 32 && c175 > 0) {
-    look_towards_right_or_left = -1;
-  }
-  else if(c175 > 32) {
-    look_towards_right_or_left = 1;
-  }
-
-
-
-  for(int i = 0; i < 63; i++) {
-    debug(String(rgb_at_y_25[i]));
-  }
-  debug("\n");
-  c25 = center_of_drivable_space(rgb_at_y_25, line_25_until_border, look_towards_right_or_left);
-  if(c25 < 32 && c25 > 0) {
-    debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32))) + "°\n");
-    steer_mean -= 2 * calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32));
-    divisor_steering += 2;
-  }
-  else if(c25 > 32) {
-    debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32))) + "°\n");
-    steer_mean += 2 * calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32));
-    divisor_steering += 2;
-  }
-  else if(c25 == 32) {
-    debug("steer straight, perfectly centered\n");
-  }
-  else if(c25 <= 0) {
-    debug("no line detected\n");
-  }
-  debug("line till border: " + String(line_25_until_border));
-  
-
   debug("\n");
   for(int i = 0; i < 63; i++) {
     debug(String(rgb_at_y_75[i]));
   }
-  debug("\n");
-  c75 = center_of_drivable_space(rgb_at_y_75, line_75_until_border, look_towards_right_or_left);
-  if(c75 < 32 && c75 > 0) {
-    debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32))) + "°\n");
-    steer_mean -= 3 * calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32));
-    divisor_steering += 3;
+
+  uint8_t number_of_lines = count_lines(rgb_at_y_75);
+  debug("number of lines: " + String(number_of_lines) + "\n");
+  if(number_of_lines == 2) {
+    currently_seeing_both_lines = true;
+    currently_seeing_only_left_line = false;
+    currently_seeing_only_right_line = false;
+    update_indexes_of_lines(rgb_at_y_75);
+    debug("index_of_left_line: " + String(index_of_left_line) + "\n");
+    debug("index_of_right_line: " + String(index_of_right_line) + "\n");
+  } else if(number_of_lines == 1) {
+    detect_if_left_or_right_line(rgb_at_y_75);
+  } else if(number_of_lines == 0) {
+    currently_seeing_both_lines = false;
+    currently_seeing_only_left_line = false;
+    currently_seeing_only_right_line = false;
   }
-  else if(c75 > 32) {
-    debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32))) + "°\n");
-    steer_mean += 3 * calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32));
-    divisor_steering += 3;
+
+  int center = 0;
+  if(currently_seeing_both_lines) {
+    debug("currently seeing both lines!\n");
+    center = (index_of_left_line + index_of_right_line) / 2;
   }
-  else if(c75 == 32)
-    debug("steer straight, perfectly centered\n");
-  else if(c75 <= 0)
-    debug("no line detected\n");
-  debug("line till border: " + String(line_75_until_border));
-
-  debug("\n");
-  for(int i = 0; i < 63; i++) {
-    debug(String(rgb_at_y_125[i]));
+  if(currently_seeing_only_left_line) {
+    debug("currently seeing only left line\n");
+    center = index_of_left_line + 27;
   }
-  debug("\n");
-  c125 = center_of_drivable_space(rgb_at_y_125, line_125_until_border, look_towards_right_or_left);
-  if(c125 < 32 && c125 > 0) {
-    debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32))) + "°\n");
-    steer_mean -= calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32));
-    divisor_steering += 1;
+  if(currently_seeing_only_right_line) {
+    debug("currently seeing only right line\n");
+    center = index_of_right_line - 27;
   }
-  else if(c125 > 32) {
-    debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32))) + "°\n");
-    steer_mean += calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32));
-    divisor_steering += 1;
-  }
-  else if(c125 == 32)
-    debug("steer straight, perfectly centered\n");
-  else if(c125 <= 0)
-    debug("no line detected\n");
-  debug("line till border: " + String(line_125_until_border));
 
 
 
-
-  debug("\n");
-  for(int i = 0; i < 63; i++) {
-    debug(String(rgb_at_y_175[i]));
-  }
-  debug("\n");
-  if(c175 < 32 && c175 > 0) {
-    debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32))) + "°\n");
-    steer_mean -= calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32));
-    divisor_steering += 1;
-  }
-  else if(c175 > 32) {
-    debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32))) + "°\n");
-    steer_mean += calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32));
-    divisor_steering += 1;
-  }
-  else if(c175 == 32)
-    debug("steer straight, perfectly centered\n");
-  else if(c175 <= 0)
-    debug("no line detected\n");
-  debug("line till border: " + String(line_175_until_border) + "\n");*/
-
-
-
-
-
-  int steer_angle = steer_mean / divisor_steering;
-    
-  debug("\nsteer_angle: " + String(steer_angle) + "\n");
-
-  uint8_t number_of_positive_1 = 0;
-  uint8_t number_of_negative_1 = 0;
-  if(line_25_until_border == 1)
-    number_of_positive_1++;
-  if(line_75_until_border == 1)
-    number_of_positive_1++;
-  if(line_125_until_border == 1)
-    number_of_positive_1++;
-  if(line_175_until_border == 1)
-    number_of_positive_1++;
-    
-  if(line_25_until_border == -1)
-    number_of_negative_1++;
-  if(line_75_until_border == -1)
-    number_of_negative_1++;
-  if(line_125_until_border == -1)
-    number_of_negative_1++;
-  if(line_175_until_border == -1)
-    number_of_negative_1++;
-
-  if(line_25_until_border != 0 && c25 != 0) {
-    if(number_of_positive_1 > number_of_negative_1) {
-      steer_angle = steer_angle * 2;
-      debug("probably in right curve, doubling steer_angle\n");
-      if(steer_angle < 0)
-        steer_angle = steer_angle * -1;
-    } else if(number_of_positive_1 < number_of_negative_1) {
-      steer_angle = steer_angle * 2;
-      debug("probably in left curve, doubling steer_angle\n");
-      if(steer_angle > 0)
-        steer_angle = steer_angle * -1;
+  if(number_of_lines > 0) {
+    int steer_angle = calculate_pull_towards_ideallinie_in_degrees(abs(center - 32));
+    if(center >= 32)
+      debug("steer right: " + String(steer_angle) + "\n");
+    else {
+      debug("steer left: " + String(steer_angle) + "\n");
+      steer_angle = steer_angle * -1;
     }
-  } else debug("not checking for curve, upper line sees straight\n");
-
-  /*if((c175 > 32 && steer_angle < 0) || (c175 < 32 && c175 > 0 && steer_angle > 0)) {
-    steer_angle = steer_angle * -1;
-    debug("flipped steer_angle, listening to nearest line");
-    steer_angle = steer_angle * 2;
-  }*/
-
-  if(abs(c175 - c25) < 10) { // on straight
-    steer_angle /= 5;
-  }
-
-  if(steer_angle > 450)
-    steer_angle = 450;
-  else if(steer_angle < -450)
-    steer_angle = -450;
 
 
-  if(steer_angle < 0) { // steer left
+    /*c175 = center_of_drivable_space(rgb_at_y_175, line_175_until_border, look_towards_right_or_left);
+    if(c175 < 32 && c175 > 0) {
+      look_towards_right_or_left = -1;
+    }
+    else if(c175 > 32) {
+      look_towards_right_or_left = 1;
+    }
+
+
+
+    for(int i = 0; i < 63; i++) {
+      debug(String(rgb_at_y_25[i]));
+    }
+    debug("\n");
+    c25 = center_of_drivable_space(rgb_at_y_25, line_25_until_border, look_towards_right_or_left);
+    if(c25 < 32 && c25 > 0) {
+      debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32))) + "°\n");
+      steer_mean -= 2 * calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32));
+      divisor_steering += 2;
+    }
+    else if(c25 > 32) {
+      debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32))) + "°\n");
+      steer_mean += 2 * calculate_pull_towards_ideallinie_in_degrees(abs(c25 - 32));
+      divisor_steering += 2;
+    }
+    else if(c25 == 32) {
+      debug("steer straight, perfectly centered\n");
+    }
+    else if(c25 <= 0) {
+      debug("no line detected\n");
+    }
+    debug("line till border: " + String(line_25_until_border));
+    
+
+    debug("\n");
+    for(int i = 0; i < 63; i++) {
+      debug(String(rgb_at_y_75[i]));
+    }
+    debug("\n");
+    c75 = center_of_drivable_space(rgb_at_y_75, line_75_until_border, look_towards_right_or_left);
+    if(c75 < 32 && c75 > 0) {
+      debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32))) + "°\n");
+      steer_mean -= 3 * calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32));
+      divisor_steering += 3;
+    }
+    else if(c75 > 32) {
+      debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32))) + "°\n");
+      steer_mean += 3 * calculate_pull_towards_ideallinie_in_degrees(abs(c75 - 32));
+      divisor_steering += 3;
+    }
+    else if(c75 == 32)
+      debug("steer straight, perfectly centered\n");
+    else if(c75 <= 0)
+      debug("no line detected\n");
+    debug("line till border: " + String(line_75_until_border));
+
+    debug("\n");
+    for(int i = 0; i < 63; i++) {
+      debug(String(rgb_at_y_125[i]));
+    }
+    debug("\n");
+    c125 = center_of_drivable_space(rgb_at_y_125, line_125_until_border, look_towards_right_or_left);
+    if(c125 < 32 && c125 > 0) {
+      debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32))) + "°\n");
+      steer_mean -= calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32));
+      divisor_steering += 1;
+    }
+    else if(c125 > 32) {
+      debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32))) + "°\n");
+      steer_mean += calculate_pull_towards_ideallinie_in_degrees(abs(c125 - 32));
+      divisor_steering += 1;
+    }
+    else if(c125 == 32)
+      debug("steer straight, perfectly centered\n");
+    else if(c125 <= 0)
+      debug("no line detected\n");
+    debug("line till border: " + String(line_125_until_border));
+
+
+
+
+    debug("\n");
+    for(int i = 0; i < 63; i++) {
+      debug(String(rgb_at_y_175[i]));
+    }
+    debug("\n");
+    if(c175 < 32 && c175 > 0) {
+      debug("steer left, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32))) + "°\n");
+      steer_mean -= calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32));
+      divisor_steering += 1;
+    }
+    else if(c175 > 32) {
+      debug("steer right, angle: " + String(calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32))) + "°\n");
+      steer_mean += calculate_pull_towards_ideallinie_in_degrees(abs(c175 - 32));
+      divisor_steering += 1;
+    }
+    else if(c175 == 32)
+      debug("steer straight, perfectly centered\n");
+    else if(c175 <= 0)
+      debug("no line detected\n");
+    debug("line till border: " + String(line_175_until_border) + "\n");
+
+
+
+
+
+    int steer_angle = steer_mean / divisor_steering;
+      
+    debug("\nsteer_angle: " + String(steer_angle) + "\n");
+
+    uint8_t number_of_positive_1 = 0;
+    uint8_t number_of_negative_1 = 0;
+    if(line_25_until_border == 1)
+      number_of_positive_1++;
+    if(line_75_until_border == 1)
+      number_of_positive_1++;
+    if(line_125_until_border == 1)
+      number_of_positive_1++;
+    if(line_175_until_border == 1)
+      number_of_positive_1++;
+      
+    if(line_25_until_border == -1)
+      number_of_negative_1++;
+    if(line_75_until_border == -1)
+      number_of_negative_1++;
+    if(line_125_until_border == -1)
+      number_of_negative_1++;
+    if(line_175_until_border == -1)
+      number_of_negative_1++;
+
+    if(line_25_until_border != 0 && c25 != 0) {
+      if(number_of_positive_1 > number_of_negative_1) {
+        steer_angle = steer_angle * 2;
+        debug("probably in right curve, doubling steer_angle\n");
+        if(steer_angle < 0)
+          steer_angle = steer_angle * -1;
+      } else if(number_of_positive_1 < number_of_negative_1) {
+        steer_angle = steer_angle * 2;
+        debug("probably in left curve, doubling steer_angle\n");
+        if(steer_angle > 0)
+          steer_angle = steer_angle * -1;
+      }
+    } else debug("not checking for curve, upper line sees straight\n");
+
+
+    if(abs(c175 - c25) < 10) { // on straight
+      steer_angle /= 5;
+    }
+  */
+    if(steer_angle > 450)
+      steer_angle = 450;
+    else if(steer_angle < -450)
+      steer_angle = -450;
+
+
+    if(steer_angle < 0) { // steer left
       //if(last_steering_direction == "right")
         //steer_angle = steer_angle / 2;
         
@@ -470,7 +566,7 @@ void loop() {
           current_angle_string = "L"+String(abs(steer_angle))+"S";
       }
     }
-    else { // steer right      
+    else if(steer_angle > 0) { // steer right      
       //if(last_steering_direction == "left")
         //steer_angle = steer_angle / 2;
         
@@ -490,10 +586,13 @@ void loop() {
         else
           current_angle_string = "R"+String(steer_angle)+"S";
       }
+    } else { // straight
+      current_angle_string = "S000S";
     }
 
 
-  write_i2c(current_angle_string);
-  debug("\n");
-  debug("\n");
+    write_i2c(current_angle_string);
+    debug("\n");
+    debug("\n");
+  }
 }
